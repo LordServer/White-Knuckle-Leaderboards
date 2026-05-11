5fc20aa3dda6c0909847304df02e595ee078fa12
Maxwell A. Stiles <MaxwellStiles@Stiles.Casa>
2026-05-11 03:40:02 +0000

    setup index

--- src/Controller/SubcategoryController.php
+++ src/Controller/SubcategoryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\SubcategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,13 +11,15 @@ use Symfony\Component\Routing\Attribute\Route;
 final class SubcategoryController extends AbstractController
 {
     #[Route('/', name: 'index')]
-    public function index(): Response
+    public function index(SubcategoryRepository $subcategoryRepository): Response
     {
         $user = $this->getUser();
+        $subcategories = $subcategoryRepository->findAll();
 
         return $this->render('subcategory/index.html.twig', [
             'controller_name' => 'SubcategoryController',
             'user' => $user,
+            'subcategories' => $subcategories,
         ]);
     }
 
