6d24aa6b0e9571031066955e9ff2cec6aa36023a
Maxwell A. Stiles <MaxwellStiles@Stiles.Casa>
2026-05-12 03:42:17 +0000

    updated schema

--- src/Form/CategoryType.php
+++ src/Form/CategoryType.php
@@ -8,8 +8,6 @@ use App\Entity\Subcategory;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -18,24 +16,20 @@ class CategoryType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name', TextType::class, [
-                'required' => true,
-            ])
-            ->add('rules', TextareaType::class, [
-                'required' => true,
-            ])
-            ->add('is_archived', CheckboxType::class, [
+            ->add('name')
+            ->add('rules')
+            ->add('isArchived', CheckboxType::class, [
                 'required' => false,
             ])
             ->add('subcategory', EntityType::class, [
                 'class' => Subcategory::class,
                 'choice_label' => 'name',
-                'required' => true,
+                'multiple' => true,
             ])
-            ->add('rank_method', EntityType::class, [
+            ->add('rankMethod', EntityType::class, [
                 'class' => RankMethod::class,
                 'choice_label' => 'name',
-                'required' => true,
+                'multiple' => true,
             ])
         ;
     }
@@ -44,9 +38,6 @@ class CategoryType extends AbstractType
     {
         $resolver->setDefaults([
             'data_class' => Category::class,
-            'csrf_protection' => true,
-            'csrf_field_name' => '_token',
-            'csrf_token_id' => 'subcategory_item',
         ]);
     }
 }
