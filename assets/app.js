6e09f7563e36eb85c5698246aec07f5a47802335
Maxwell A. Stiles <MaxwellStiles@Stiles.Casa>
2026-05-13 00:11:21 +0000

    added ability to make dropdown boxes links

--- assets/app.js
+++ assets/app.js
@@ -9,3 +9,14 @@ import './stimulus_bootstrap.js';
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
 import '@tailwindplus/elements';
+
+document.addEventListener('turbo:load', function() {
+    const selector = document.getElementById('form-control');
+
+    selector.addEventListener('change', function() {
+        const url = this.value;
+        if (url) {
+            window.location.href = url; // Redirects to the selected route
+        }
+    });
+});
