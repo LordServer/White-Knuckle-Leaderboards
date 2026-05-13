a85a45f6a40e5bdb85b5c54e4950eca6f4baf170
Maxwell A. Stiles <MaxwellStiles@Stiles.Casa>
2026-05-13 00:11:01 +0000

    initial commit

---
+++ src/Controller/LeaderboardController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\CategoryRepository;
+use App\Repository\SubcategoryRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class LeaderboardController extends AbstractController
+{
+    #[Route('/leaderboard/{categoryId<\d+>?1}/{subcategoryId<\d+>?1}', name: 'app_leaderboard')]
+    public function index(int $categoryId, int $subcategoryId, CategoryRepository $categoryRepository, SubcategoryRepository $subcategoryRepository): Response
+    {
+        $user = $this->getUser();
+        $categories = $categoryRepository->findAll();
+        $category = $categoryRepository->findOneBy(['id' => $categoryId]);
+        $subcategory = $subcategoryRepository->findOneBy(['id' => $subcategoryId]);
+
+        return $this->render('leaderboard/index.html.twig', [
+            'controller_name' => 'LeaderboardController',
+            'user' => $user,
+            'categories' => $categories,
+            'category' => $category,
+            'subcategory' => $subcategory,
+        ]);
+    }
+}
