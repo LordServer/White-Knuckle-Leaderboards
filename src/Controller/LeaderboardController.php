a7b4f61c98e12a5f162b64402d294e1271dbb440
Maxwell A. Stiles <MaxwellStiles@Stiles.Casa>
2026-05-21 02:53:22 +0000

    get climbs that are ranked

--- src/Controller/LeaderboardController.php
+++ src/Controller/LeaderboardController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\CategoryRepository;
+use App\Repository\ClimbRepository;
 use App\Repository\SubcategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,17 +12,20 @@ use Symfony\Component\Routing\Attribute\Route;
 final class LeaderboardController extends AbstractController
 {
     #[Route('/leaderboard/{categoryId<\d+>?1}/{subcategoryId<\d+>?1}', name: 'app_leaderboard')]
-    public function index(int $categoryId, int $subcategoryId, CategoryRepository $categoryRepository, SubcategoryRepository $subcategoryRepository): Response
+    public function index(int $categoryId, int $subcategoryId, CategoryRepository $categoryRepository, SubcategoryRepository $subcategoryRepository, ClimbRepository $climbRepository): Response
     {
         $categories = $categoryRepository->findAll();
         $category = $categoryRepository->findOneBy(['id' => $categoryId]);
         $subcategory = $subcategoryRepository->findOneBy(['id' => $subcategoryId]);
+
+        $climbs = $climbRepository->findByCategoryAndSubcategoryAndRankSortByRank($category, $subcategory);
 
         return $this->render('leaderboard/index.html.twig', [
             'controller_name' => 'LeaderboardController',
             'categories' => $categories,
             'category' => $category,
             'subcategory' => $subcategory,
+            'climbs' => $climbs,
         ]);
     }
 }
