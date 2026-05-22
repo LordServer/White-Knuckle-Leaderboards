c249cb8b486d34ee6175a3367cb96eab53ba48a5
Maxwell A. Stiles <MaxwellStiles@Stiles.Casa>
2026-05-22 22:11:48 +0000

    setup climber climbs page

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\CategoryRepository;
+use App\Repository\ClimbRepository;
 use App\Repository\UserRepository;
 use App\Service\Breadcrumbs;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -49,7 +51,7 @@ final class UserController extends AbstractController
         $climber = $userRepository->findOneBy(['id' => $userId]);
 
         if (!$climber) {
-            throw $this->createNotFoundException();
+            throw $this->createNotFoundException('Climber not found');
         }
 
         $breadcrumbs
@@ -66,10 +68,39 @@ final class UserController extends AbstractController
     }
 
     #[Route('/read/{userId<\d+>}/climbs/{categoryId<\d+>?1}/{subcategoryId<\d+>?1}', name: 'climbs')]
-    public function climbs(int $userId, int $categoryId, int $subcategoryId): Response
+    public function climbs(int $userId, int $categoryId, int $subcategoryId, UserRepository $userRepository, CategoryRepository $categoryRepository, ClimbRepository $climbRepository): Response
     {
+        $climber = $userRepository->findOneBy(['id' => $userId]);
+
+        if (!$climber) {
+            throw $this->createNotFoundException('Climber not found');
+        }
+
+        $categories = $categoryRepository->findAll();
+
+        $category = current(array_filter(
+            $categories,
+            fn ($category) => $category->getId() === $categoryId
+        ));
+        $category = $category ?: ($categories[0] ?? null);
+
+        $subcategory = current(array_filter(
+            $category->getSubcategory()->toArray(),
+            fn ($subcategory) => $subcategory->getId() === $subcategoryId
+        ));
+        $subcategory = $subcategory ?: ($category->getSubcategory()->toArray()[0] ?? null);
+
+        $climbs = $climbRepository->findByUserAndCategoryAndSubcategoryOrderByNewestCreatedAt($climber, $category, $subcategory);
+
+        // TODO: add breadcrumbs
+
         return $this->render('user/climbs.html.twig', [
             'controller_name' => 'UserController',
+            'climber' => $climber,
+            'categories' => $categories,
+            'category' => $category,
+            'subcategory' => $subcategory,
+            'climbs' => $climbs,
         ]);
     }
 
