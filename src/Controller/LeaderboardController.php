ce2317589c79128ba3a5cf6fa221f0fd60d705a1
Maxwell A. Stiles <MaxwellStiles@Stiles.Casa>
2026-05-22 20:06:51 +0000

    setup archive page

--- src/Controller/LeaderboardController.php
+++ src/Controller/LeaderboardController.php
@@ -13,7 +13,7 @@ use Symfony\Component\Routing\Attribute\Route;
 final class LeaderboardController extends AbstractController
 {
     #[Route('/leaderboard/{categoryId<\d+>?1}/{subcategoryId<\d+>?1}', name: 'app_leaderboard')]
-    public function index(int $categoryId, int $subcategoryId, CategoryRepository $categoryRepository, SubcategoryRepository $subcategoryRepository, ClimbRepository $climbRepository, Breadcrumbs $breadcrumbs): Response
+    public function leaderboard(int $categoryId, int $subcategoryId, CategoryRepository $categoryRepository, SubcategoryRepository $subcategoryRepository, ClimbRepository $climbRepository, Breadcrumbs $breadcrumbs): Response
     {
         $categories = $categoryRepository->findByArchived(false);
         $category = $categoryRepository->findOneBy(['id' => $categoryId]);
@@ -39,7 +39,45 @@ final class LeaderboardController extends AbstractController
             ->add($pageName, 'app_leaderboard', ['categoryId' => $category->getId(), 'subcategoryId' => $subcategory->getId()])
         ;
 
-        return $this->render('leaderboard/index.html.twig', [
+        return $this->render('leaderboard/leaderboard.html.twig', [
+            'controller_name' => 'LeaderboardController',
+            'categories' => $categories,
+            'category' => $category,
+            'subcategory' => $subcategory,
+            'climbs' => $climbs,
+            'breadcrumbs' => $breadcrumbs->all(),
+            'pageName' => $pageName,
+        ]);
+    }
+
+    #[Route('/archive/{categoryId<\d+>?1}/{subcategoryId<\d+>?1}', name: 'app_archive')]
+    public function archive(int $categoryId, int $subcategoryId, CategoryRepository $categoryRepository, SubcategoryRepository $subcategoryRepository, ClimbRepository $climbRepository, Breadcrumbs $breadcrumbs): Response
+    {
+        $categories = $categoryRepository->findByArchived(true);
+        $category = $categoryRepository->findOneBy(['id' => $categoryId]);
+        if (!$category) {
+            $category = $categoryRepository->findOneBy(['id' => min(array_map(fn ($item) => $item->getId(), $categories))]);
+        }
+        $subcategory = $subcategoryRepository->findOneBy(['id' => $subcategoryId]);
+        if (!$category->getSubcategory()->contains($subcategory)) {
+            $subcategory = $subcategoryRepository->findOneBy(['id' => min(array_map(fn ($item) => $item->getId(), $category->getSubcategory()->toArray()))]);
+        }
+
+        $climbs = $climbRepository->findByCategoryAndSubcategoryAndRankSortByRank($category, $subcategory);
+
+        if ('Normal' === $subcategory->getName()) {
+            $pageName = $category->getName();
+        } else {
+            $pageName = $subcategory->getName().' '.$category->getName();
+        }
+
+        $breadcrumbs
+            ->addHome()
+            ->add('Archive', 'app_archive')
+            ->add($pageName, 'app_archive', ['categoryId' => $category->getId(), 'subcategoryId' => $subcategory->getId()])
+        ;
+
+        return $this->render('leaderboard/archive.html.twig', [
             'controller_name' => 'LeaderboardController',
             'categories' => $categories,
             'category' => $category,
