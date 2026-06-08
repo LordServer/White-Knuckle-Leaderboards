c1620dc41eaccd674d66ff00da8be56514870d84
Maxwell A. Stiles <MaxwellStiles@Stiles.Casa>
2026-06-08 02:34:22 +0000

    databse migration

---
+++ migrations/Version20260608014718.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260608014718 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE api_token ADD description TEXT DEFAULT NULL');
+        $this->addSql('ALTER TABLE api_token ADD last_used TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE api_token DROP description');
+        $this->addSql('ALTER TABLE api_token DROP last_used');
+    }
+}
