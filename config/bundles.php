b8b50cafb7e23b14cb635642a64d3439ffe616c5
Maxwell A. Stiles <MaxwellStiles@Stiles.Casa>
2026-06-10 07:17:16 +0000

    composer require jolicode/automapper

--- config/bundles.php
+++ config/bundles.php
@@ -23,4 +23,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
     BabDev\PagerfantaBundle\BabDevPagerfantaBundle::class => ['all' => true],
+    AutoMapper\Symfony\Bundle\AutoMapperBundle::class => ['all' => true],
 ];
