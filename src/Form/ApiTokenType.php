33059f329e2302366a1a85bdcad3a721784d832e
Maxwell A. Stiles <MaxwellStiles@Stiles.Casa>
2026-06-11 18:43:48 +0000

    setup for type for apiTokens

---
+++ src/Form/ApiTokenType.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\ApiToken;
+use App\Service\ScopePermissionService;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ApiTokenType extends AbstractType
+{
+    public function __construct(
+        private readonly ScopePermissionService $scopePermissionService,
+    ) {
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('isEnabled', CheckboxType::class, [
+                'required' => false,
+            ])
+            ->add('description')
+        ;
+
+        $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) {
+            $token = $event->getData();
+            if (!$token || null === $token->getId()) {
+                $event->getForm()
+                    ->add('token')
+                    ->add('scopes', ChoiceType::class, [
+                        'choices' => $this->scopePermissionService->getAssignableScopes(),
+                        'multiple' => true,
+                    ])
+                    ->add('expiresAt', DateTimeType::class, [
+                        'data' => new \DateTimeImmutable('+1 year'),
+                        'required' => false,
+                        'attr' => [
+                            'data-token-expiry-target' => 'expiresAt',
+                        ],
+                    ])
+                    ->add('neverExpires', CheckboxType::class, [
+                        'mapped' => false,
+                        'required' => false,
+                        'attr' => [
+                            'data-token-expiry-target' => 'checkbox',
+                            'data-action' => 'change->token-expiry#toggle',
+                        ],
+                    ]);
+            }
+        });
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => ApiToken::class,
+        ]);
+    }
+}
