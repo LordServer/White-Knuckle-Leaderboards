c893d24e56bf70584db0043fcf93d118b7a0b785
Maxwell A. Stiles <MaxwellStiles@Stiles.Casa>
2026-06-11 01:06:29 +0000

    moved api requests into own firewall with custom handler

---
+++ src/Security/ApiTokenHandler.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Security;
+
+use App\Repository\ApiTokenRepository;
+use Symfony\Component\Security\Core\Exception\BadCredentialsException;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
+use Symfony\Component\Security\Http\AccessToken\AccessTokenHandlerInterface;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+
+readonly class ApiTokenHandler implements AccessTokenHandlerInterface
+{
+    public function __construct(
+        private ApiTokenRepository $apiTokenRepository,
+    ) {
+    }
+
+    public function getUserBadgeFrom(#[\SensitiveParameter] string $accessToken): UserBadge
+    {
+        $token = $this->apiTokenRepository->findOneBy(['token' => $accessToken]);
+
+        if (!$token) {
+            throw new BadCredentialsException();
+        }
+
+        if (!$token->isValid()) {
+            throw new CustomUserMessageAuthenticationException('Token expired.');
+        }
+
+        if (!$token->isEnabled()) {
+            throw new CustomUserMessageAuthenticationException('Token not enabled.');
+        }
+
+        return new UserBadge($token->getOwnedBy()->getUserIdentifier());
+    }
+}
