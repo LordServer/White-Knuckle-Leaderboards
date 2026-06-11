b4d5e3be6e41e33ea8342f3ce70897f0755743a4
Maxwell A. Stiles <MaxwellStiles@Stiles.Casa>
2026-06-11 18:44:54 +0000

    added voter checking for page access

--- src/Controller/ApiTokensController.php
+++ src/Controller/ApiTokensController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\ApiToken;
+use App\Form\ApiTokenType;
 use App\Repository\ApiTokenRepository;
 use App\Repository\UserRepository;
+use App\Security\Voter\ApiTokenVoter;
 use App\Service\BreadcrumbsService;
 use App\Service\PaginationService;
 use App\Service\ScopePermissionService;
@@ -18,6 +21,8 @@ final class ApiTokensController extends AbstractController
     #[Route('/', name: 'index')]
     public function apiTokens(ApiTokenRepository $apiTokenRepository, BreadcrumbsService $breadcrumbs, Request $request, PaginationService $paginationService, UserRepository $userRepository): Response
     {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
         $apiTokens = $apiTokenRepository->findByOwnerOrderByIndex($this->getUser());
         $apiTokens->setMaxPerPage($request->query->get('perPage', 50));
         $apiTokens->setCurrentPage($request->query->get('page', 1));
@@ -44,35 +49,58 @@ final class ApiTokensController extends AbstractController
     }
 
     #[Route('/create', name: 'create')]
-    public function apiTokensCreate(ScopePermissionService $scopes): Response
+    public function apiTokensCreate(ScopePermissionService $scopesManager): Response
     {
-        $apiScopes = $scopes->getAssignableScopes();
+        $apiToken = new ApiToken();
+
+        $this->denyAccessUnlessGranted(ApiTokenVoter::CREATE, $apiToken);
+
+        $apiScopes = $scopesManager->getAssignableScopes();
+
+        $form = $this->createForm(ApiTokenType::class, $apiToken);
 
         return $this->render('api_tokens/create.html.twig', [
             'controller_name' => 'ApiTokensController',
             'apiScopes' => $apiScopes,
+            'form' => $form,
         ]);
     }
 
     #[Route('/{apiTokenId<\d+>}', name: 'read')]
-    public function apiTokensRead(): Response
+    public function apiTokensRead(int $apiTokenId, ApiTokenRepository $apiTokenRepository): Response
     {
+        $apiToken = $apiTokenRepository->findOneBy(['id' => $apiTokenId]);
+
+        $this->denyAccessUnlessGranted(ApiTokenVoter::READ, $apiToken);
+
         return $this->render('api_tokens/read.html.twig', [
             'controller_name' => 'ApiTokensController',
+            'apiToken' => $apiToken,
         ]);
     }
 
     #[Route('/{apiTokenId<\d+>}/update', name: 'update')]
-    public function apiTokensUpdate(): Response
+    public function apiTokensUpdate(int $apiTokenId, ApiTokenRepository $apiTokenRepository): Response
     {
+        $apiToken = $apiTokenRepository->findOneBy(['id' => $apiTokenId]);
+
+        $this->denyAccessUnlessGranted(ApiTokenVoter::UPDATE, $apiToken);
+
+        $form = $this->createForm(ApiTokenType::class, $apiToken);
+
         return $this->render('api_tokens/update.html.twig', [
             'controller_name' => 'ApiTokensController',
+            'form' => $form,
         ]);
     }
 
     #[Route('/{apiTokenId<\d+>}/delete', name: 'delete')]
-    public function apiTokensDelete(): Response
+    public function apiTokensDelete(int $apiTokenId, ApiTokenRepository $apiTokenRepository): Response
     {
+        $apiToken = $apiTokenRepository->findOneBy(['id' => $apiTokenId]);
+
+        $this->denyAccessUnlessGranted(ApiTokenVoter::UPDATE, $apiToken);
+
         return $this->render('api_tokens/delete.html.twig', [
             'controller_name' => 'ApiTokensController',
         ]);
