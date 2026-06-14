0e0190dc5116d6bd606e44eca39259e6f6c655d2
Maxwell A. Stiles <MaxwellStiles@Stiles.Casa>
2026-06-14 03:42:42 +0000

    fixed creating new entity instead of updating existing

--- src/State/EntityClassDtoStateProcessor.php
+++ src/State/EntityClassDtoStateProcessor.php
@@ -9,14 +9,16 @@ use ApiPlatform\Metadata\DeleteOperationInterface;
 use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProcessorInterface;
 use AutoMapper\AutoMapperInterface;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
 
-class EntityClassDtoStateProcessor implements ProcessorInterface
+readonly class EntityClassDtoStateProcessor implements ProcessorInterface
 {
     public function __construct(
         #[Autowire(service: PersistProcessor::class)] private ProcessorInterface $persistProcessor,
         #[Autowire(service: RemoveProcessor::class)] private ProcessorInterface $removeProcessor,
         private AutoMapperInterface $autoMapper,
+        private EntityManagerInterface $entityManager,
     ) {
     }
 
@@ -26,7 +28,7 @@ class EntityClassDtoStateProcessor implements ProcessorInterface
         assert($stateOptions instanceof Options);
         $entityClass = $stateOptions->getEntityClass();
 
-        $entity = $this->mapDtoToEntity($data, $entityClass);
+        $entity = $this->mapDtoToEntity($data, $entityClass, $uriVariables);
 
         if ($operation instanceof DeleteOperationInterface) {
             $this->removeProcessor->process($entity, $operation, $uriVariables, $context);
@@ -40,26 +42,22 @@ class EntityClassDtoStateProcessor implements ProcessorInterface
         return $data;
     }
 
-    private function mapDtoToEntity(object $dto, string $entityClass): object
-    {
-        //        assert($dto instanceof UserApi);
-        //        if ($dto->id) {
-        //            $entity = $this->userRepository->find($dto->id);
-        //
-        //            if (!$entity) {
-        //                throw new \Exception(sprintf('User with id "%s" not found.', $dto->id));
-        //            }
-        //        } else {
-        //            $entity = new User();
-        //        }
-        //
-        //        $entity->setUsername($dto->username);
-        //        $entity->setDiscordId($dto->discord_id);
-        //        $entity->setAvatar($dto->avatar);
-        //        $entity->setDisplayName($dto->display_name);
-        //        $entity->setStatus(UserStatus::ACTIVE);
-        //
-        //        return $entity;
+    private function mapDtoToEntity(
+        object $dto,
+        string $entityClass,
+        array $uriVariables,
+    ): object {
+        if (isset($uriVariables['id'])) {
+            $entity = $this->entityManager
+                ->getRepository($entityClass)
+                ->find($uriVariables['id']);
+
+            if ($entity) {
+                $this->autoMapper->map($dto, $entity);
+
+                return $entity;
+            }
+        }
 
         return $this->autoMapper->map($dto, $entityClass);
     }
